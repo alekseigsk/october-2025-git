a93467a784a6296cb9f3d990622f7e731ae6ae66
alekseigsk <alekseigsk@gmail.com>
2025-12-02 01:54:01 +0000

    added bash script files

---
+++ hello/hello.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function file() {
+	touch hello/file4
+	touch hello/file5
+}
+
+function folder() {
+	mkdir hello
+	mkdir world
+}
+
+function tree () {
+	sudo yum install tree -y
+	sudo yum install unzip -y > /dev/null
+}
+
+folder
+file
+tree
